323a74a7e14d4d6a6cf58bdc766dc1ea5b5ef439
Furkan Gözükara <furkangozukara@gmail.com>
2022-10-20 15:56:07 +0300

    lecture 3 part 4

--- lecture 3/lecture 3/MainWindow.xaml.cs
+++ lecture 3/lecture 3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -30,7 +31,7 @@ namespace lecture_3
         List<Tuple<int, string>> ListTuples = new List<Tuple<int, string>>();
         Dictionary<int, string> DictTuples = new Dictionary<int, string>();
 
-        private void speedTest_Click(object sender, RoutedEventArgs e)
+        private void btnnInitTestdata_Click(object sender, RoutedEventArgs e)
         {
             //generate 1 random number
             //add i and that random number to the both list and dictionary
@@ -78,13 +79,27 @@ namespace lecture_3
             string srPath = "default_sort_list.txt";
             int irNewNumber = 55;
             int irSecondNumber = 100;
-            writeListToTextFile(srPath, ListTuples , irNewNumber, ref irSecondNumber);
+
+            //   writeListToTextFile_3(srPath, ListTuples, irNewNumber, ref irSecondNumber);
+
+            writeListToTextFile(srPath, ListTuples, irNewNumber, ref irSecondNumber);
             //string doesnt behave like a class type because it is immutable
+
+            //       writeListToTextFile("default_sort_list.txt", ListTuples, irNewNumber, ref irSecondNumber);
+
+            //  writeListToTextFile_v2("default_sort_list.txt", ref ListTuples, irNewNumber, ref irSecondNumber);
+
             ListTuples = ListTuples.OrderBy(anyThing => Guid.NewGuid()).ToList();
 
-            writeListToTextFile("default_sort_list.txt", ListTuples, irNewNumber, ref irSecondNumber);
+            writeListToTextFile("random_sorted_list.txt", ListTuples, irNewNumber, ref irSecondNumber);
+            //string doesnt behave like a class type because it is immutable
 
-            writeListToTextFile_v2("default_sort_list.txt", ref ListTuples, irNewNumber, ref irSecondNumber);
+            writeDictioaryToFile("default_sorted_dic.txt", DictTuples);
+
+            DictTuples = DictTuples.OrderBy(anyThing => Guid.NewGuid()).ToDictionary(anyThing => anyThing.Key, anyThing => anyThing.Value);
+
+            writeDictioaryToFile("random_sorted_dic.txt", DictTuples);
+
         }
 
         Tuple<int, int, int> doubleThem(Tuple<int, int, int> vrTuple)
@@ -93,7 +108,7 @@ namespace lecture_3
         }
 
         //complete this method
-        private void writeListToTextFile(string FileName, List<Tuple<int, string>> ListTuples , int irNewNumber, ref int irSecondNumber)
+        private void writeListToTextFile(string FileName, List<Tuple<int, string>> ListTuples, int irNewNumber, ref int irSecondNumber)
         {
             //if you dont provide a full path it will save file into the same folder as exe - where the exe is running 
             File.WriteAllLines(FileName, ListTuples.Select(perPairs => perPairs.Item1 + "\t" + perPairs.Item2));
@@ -118,6 +133,7 @@ namespace lecture_3
             //    }
             //}
 
+            //this does not copy new reference into the original reference
             ListTuples = new List<Tuple<int, string>>();
         }
 
@@ -130,7 +146,58 @@ namespace lecture_3
         {
             File.WriteAllLines(FileName, ListTuples.Select(perPairs => perPairs.Item1 + "\t" + perPairs.Item2));
 
+            //it overwrites the original reference
             ListTuples = new List<Tuple<int, string>>();
+        }
+
+        private void writeListToTextFile_3(string FileName, List<Tuple<int, string>> ListTuples, int irNewNumber, ref int irSecondNumber)
+        {
+            //how can i use ListTuples in a way that the changes i made here will not affect the original ListTuples
+
+            ListTuples = ListTuples.ToList();//this will generate a new list with a new reference therefore it will not affect original list
+
+            ListTuples[0] = new Tuple<int, string>(0, "999");//would 
+
+            ListTuples.Sort();
+        }
+
+        private void speedTest_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Random randTest = new Random();
+
+            List<int> listTestKeys = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                listTestKeys.Add(randTest.Next(0, 100));
+            }
+
+            Stopwatch swTimer = new Stopwatch();
+            swTimer.Start();
+            List<string> lstListFoundItems = new List<string>();
+            foreach (var vrKey in listTestKeys)
+            {
+                var vrFoundTuple = ListTuples.Where(pr => pr.Item1 == vrKey).FirstOrDefault();
+                if (vrFoundTuple != null)
+                    lstListFoundItems.Add(vrFoundTuple.Item1 + "\n" + vrFoundTuple.Item2);
+            }
+            swTimer.Stop();
+            MessageBox.Show("list finding took: " + swTimer.ElapsedMilliseconds.ToString("N0")+" ms");
+            File.WriteAllLines("list found items.txt", lstListFoundItems);
+
+            swTimer.Reset();
+            swTimer.Start();
+            List<string> lstDictFoundItems = new List<string>();
+            foreach (var vrKey in listTestKeys)
+            {
+                var vrFoundValue = DictTuples.ContainsKey(vrKey) ? DictTuples[vrKey] : null;
+                lstDictFoundItems.Add(vrKey + "\n" + vrFoundValue);
+            }
+            swTimer.Stop();
+            MessageBox.Show("dictionary finding took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " ms");
+            File.WriteAllLines("dict found items.txt", lstDictFoundItems);
         }
     }
 }
