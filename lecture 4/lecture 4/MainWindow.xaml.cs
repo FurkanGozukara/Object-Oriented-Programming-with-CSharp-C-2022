65a0e6a9203f5ee94cec2f212e04d4b2b9039712
Furkan Gözükara <furkangozukara@gmail.com>
2022-10-27 13:41:53 +0300

    lecture 4 part 1

---
+++ lecture 4/lecture 4/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lecture_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public class exampleGC
+        {
+            public int MyProperty { get; set; }
+            public string srProp { get; set; } = "this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a testthis is a test this is a test this is a test";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        { 
+            for (int i = 0; i < 10000000; i++)
+            {
+                Task.Factory.StartNew(() => { tastGG(); });
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+
+        private void tastGG()
+        {
+            exampleGC exampleGC = new exampleGC();
+            exampleGC.MyProperty = new Random().Next();
+            for (int i = 0; i < 10; i++)
+            {
+                exampleGC.srProp += exampleGC.srProp;
+            }
+        }
+    }
+}
