421ef24ce4b75e312b239f2fece3fe7af131e3f4
Furkan Gözükara <furkangozukara@gmail.com>
2022-10-27 14:19:22 +0300

    lecture 4 part 3

--- lecture 4/lecture 4/MainWindow.xaml.cs
+++ lecture 4/lecture 4/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,24 +34,40 @@ namespace lecture_4
             public string srProp { get; set; } = "this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a test this is a testthis is a test this is a test this is a test";
         }
 
+        //make this application responsive while running
         private void Button_Click(object sender, RoutedEventArgs e)
-        { 
-            for (int i = 0; i < 10000000; i++)
+        {
+            Debug.WriteLine("Button_Click " + Thread.CurrentThread.ManagedThreadId);
+
+            Task.Factory.StartNew(async () =>
             {
-                Task.Factory.StartNew(() => { tastGG(); });
-                System.Threading.Thread.Sleep(1);
-                if (GCList.Count > 1000)
+                for (int i = 0; i < 10000000; i++)
                 {
-                    GCList.Clear();
-                    GC.Collect();
-                }                   
-            }
+                    Debug.WriteLine("inside for loop " + Thread.CurrentThread.ManagedThreadId);
+
+
+                    //start task whenever possible so this line will be immediately executed and it will continue with executing of next line
+                    Task.Factory.StartNew(() => { testGG(); });
+
+                    //however in this case, it will wait until task is completed 
+                    await Task.Factory.StartNew(() => { testGG(); });
+
+                    System.Threading.Thread.Sleep(1);
+                    if (GCList.Count > 1000)
+                    {
+                        GCList.Clear();
+                        GC.Collect();
+                    }
+                }
+            });
         }
 
         List<exampleGC> GCList = new List<exampleGC>();
 
-        private void tastGG()
+        private void testGG()
         {
+            Debug.WriteLine("testGG " + Thread.CurrentThread.ManagedThreadId);
+
             //make this application crash due to memory error
             exampleGC exampleGC = new exampleGC();
             exampleGC.MyProperty = new Random().Next();
@@ -57,6 +75,7 @@ namespace lecture_4
             {
                 exampleGC.srProp += exampleGC.srProp;
             }
+            System.Threading.Thread.Sleep(1111);
             GCList.Add(exampleGC);
         }
     }
