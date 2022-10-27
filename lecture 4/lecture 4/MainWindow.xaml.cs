c837559096feda40fb3b15d50223e46fafce4b31
Furkan Gözükara <furkangozukara@gmail.com>
2022-10-27 15:39:44 +0300

    lecture 4 part 4

--- lecture 4/lecture 4/MainWindow.xaml.cs
+++ lecture 4/lecture 4/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading;
@@ -12,6 +14,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -77,6 +80,76 @@ namespace lecture_4
             }
             System.Threading.Thread.Sleep(1111);
             GCList.Add(exampleGC);
+        }
+
+        async private Task<string> downloadSite(string srUrl)
+        {
+            using (var client = new WebClient())
+            {
+                return client.DownloadString(new Uri(srUrl));
+            }
+        }
+
+        async private void btnDownloadAsyncString_Click(object sender, RoutedEventArgs e)
+        {
+            string srUrl = "https://www.monstermmorpg.com";
+            string code = downloadSite(srUrl).GetAwaiter().GetResult();
+
+            var vrResult2 = Task.Run(async () =>
+            {
+                string msg = await downloadSite(srUrl);
+                return msg;
+            }).Result;
+
+            var vrResult = await downloadSite(srUrl);
+
+            string srResult2;
+
+            downloadSite(srUrl)
+                .ContinueWith(t =>
+                {
+                    srResult2 = t.Result;
+                })
+                .Wait();
+
+            MessageBox.Show(code);
+        }
+
+        private async void btnParallelForEach_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            List<int> lstInts = new List<int>(Enumerable.Range(1, 100).Select(pr => rnd.Next()));
+
+            lstInts = new List<int>(Enumerable.Range(1, 100));
+
+            ParallelOptions PO = new ParallelOptions();
+            PO.MaxDegreeOfParallelism = 4;
+
+            Parallel.ForEach(lstInts, PO, async number =>
+            {
+                await debugFile(number);
+            });
+
+            long lrCounter = 0;
+            
+            //our aim is that running 10 tasks simultanously and with the order they should start
+            //so whenever a task is completed, start another task and keep running task count at 10
+           
+            for (int i = 0; i < lstInts.Count; i++)
+            {
+                 Task.Factory.StartNew(() => debugFile(i)).ContinueWith(task =>
+                 {
+                     Interlocked.Add(ref lrCounter, -1);
+                 });
+                lrCounter++;
+            }
+        }
+
+        async private Task<bool> debugFile(int irVal)
+        {
+            Debug.WriteLine(irVal);
+            System.Threading.Thread.Sleep(irVal * 100);
+            return true;
         }
     }
 }
