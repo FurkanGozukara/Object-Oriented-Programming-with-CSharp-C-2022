12b6ed960d796962f02a640249826d2cc22c0e54
Furkan Gözükara <furkangozukara@gmail.com>
2022-11-17 14:21:32 +0300

    lecture 7 part 2

--- lecture 7/lecture 7/MainWindow.xaml.cs
+++ lecture 7/lecture 7/MainWindow.xaml.cs
@@ -56,6 +56,26 @@ namespace lecture_7
             _del2 = writeToFile;
 
             testMethod(_del2);
+
+            //myDel2 _del3 = myAwesomeMethod3;//signature not match
+
+            //myDel3 _del4 = myAwesomeMethod3;//return type not match
+
+            myDel2 _del5 = testMethod2;
+            _del5 += testMethod3;
+            _del5("fds");
+        }
+
+
+
+        private void testMethod3(string sr)
+        {
+            MessageBox.Show(sr+ sr);
+        }
+
+        private void testMethod2(string sr)
+        {
+            MessageBox.Show(sr);
         }
 
         private void testMethod(myDel methods)
@@ -64,6 +84,10 @@ namespace lecture_7
         }
 
         private delegate void myDel();
+
+        private delegate void myDel2(string srMsg);
+
+        private delegate int myDel3();
 
         private void writeToFile()
         {
@@ -84,5 +108,42 @@ namespace lecture_7
         {
             MessageBox.Show("awesome method call 3");
         }
+
+        class DataStore<T>
+        {
+            public T[] data = new T[10];
+        }
+
+        class DataStore
+        {
+            public int[] data = new int[10];
+        }
+
+        private void btnSimpleGenericExample_Click(object sender, RoutedEventArgs e)
+        {
+            DataStore myData = new DataStore();
+            myData.data[3] = 32;
+
+            DataStore<long> myDataF = new DataStore<long>();
+            myDataF.data[3] = 31243894789112;
+
+            DataStore<string> myDataStr = new DataStore<string>();
+            myDataStr.data[2] = "gg wp";
+            
+            //this is boxing
+            object test = myDataStr;
+            object test2 = new List<Int16> { 231, 321, 12, 12 } ;
+
+            List<object> myList = new List<object>();
+
+            myList.Add(test);
+            myList.Add(test2);
+
+            MessageBox.Show(myList[1].ToString());
+            //unboxing
+            MessageBox.Show(((List<Int16>)myList[1])[1].ToString());
+        }
     }
+
+ 
 }
