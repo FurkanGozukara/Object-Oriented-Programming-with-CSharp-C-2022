77ef751450842c08eee82742113395909fb0a658
Furkan Gözükara <furkangozukara@gmail.com>
2022-12-29 13:33:19 +0300

    lecture 12 part 1

--- lecture 11/lecture 11/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ lecture 11/lecture 11/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -117,7 +117,7 @@ namespace lecture_11 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/lecture 11;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/lecture 11;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
---
+++ lecture 12/lecture 12/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lecture_12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            swWrite.AutoFlush= true;
+        }
+
+        private void btnDataRacing_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> lstNumbers = Enumerable.Range(1, 100).ToList();
+            foreach (var vrItemm in lstNumbers)
+            {
+                Task.Factory.StartNew(async () =>
+                {
+                    await printToFile(vrItemm);
+                });
+            }
+        }
+
+        StreamWriter swWrite = new StreamWriter("test.txt");
+        private async Task printToFile(int irNumber)
+        {
+            await swWrite.WriteLineAsync(irNumber.ToString());
+        }
+    }
+}
